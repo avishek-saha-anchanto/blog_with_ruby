b9582df3717c36f9e9c36ab306f14276da0fcab6
Avishek <avishek.saha@anchanto.com>
2024-04-15 23:26:27 +0530

    Add sign up

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   root "pages#home"
   get '/about', to:'pages#about'
   resources :articles
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
   # Defines the root path route ("/")
   # root "posts#index"
 end
